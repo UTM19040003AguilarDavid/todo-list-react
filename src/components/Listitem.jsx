e7a3bfbc5e7e73790a951c0eaf9344bcfaaf4f7b
Rafael Rangel <rafa_sander79@hotmail.com>
2023-10-10 19:25:22 -0600

    Added newtodo function

--- src/components/Listitem.jsx
+++ src/components/Listitem.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 
-function ListItem({text, onDelete}) {
+function ListItem({text, onDelete, }) {
     const [checked, setChecked] = useState(false);
     const handleCheck = () => {
         setChecked(!checked);
     }
     return (
-    <li className={`bg-indigo-800 p-2 ${checked ? 'line-through' : null}`}>{text} <input type="checkbox" checked={checked} onChange={handleCheck} />
+    <li className={`rounded-md bg-indigo-800 p-2 ${checked ? 'text-green-500 line-through' : null}`}>{text} <input type="checkbox" checked={checked} onChange={handleCheck} />
     </li>
     );
 }
--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -1,12 +1,31 @@
 import ListItem from "../components/Listitem";
+import { useState, useRef } from "react";
+
+
 
 function Todo() {
+    //useState nos devuelve un Array -->
+    const [todos, setTodos] = useState([]);
+    const inputRef = useRef(null);
+
+    const addTodo = () => {
+        const todoValue = inputRef.current.value;
+        console.log(todoValue);
+        setTodos([...todos, todoValue])
+    }
+
     return (
-        <div>
-            <ul>
-                <ListItem text="Item 1"/>
-                <ListItem text="Item 2" />
-                <ListItem text="Item 3"/>
+        <div className="flex flex-col gap-2">
+            <div className="flex gap-2">
+            <input ref={inputRef} type="text" className="rounded-md bg-slate-400" />
+            <button onClick={addTodo} className="rounded-md bg-indigo-600 px-4 py-2">add</button>
+            </div>
+            <ul className="flex flex-col gap-2">
+                {
+                    todos.map((value, index) => {
+                        return <ListItem key={index} text={value} />;
+                    }
+                )}
             </ul>
         </div>
     );
