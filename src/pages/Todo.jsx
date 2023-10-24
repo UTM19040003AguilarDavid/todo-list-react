fadacd6bbd39135a396ed370a56c04e5176bcf6d
David Aguilar <utm19040003@utma.edu.mx>
2023-10-23 18:36:02 -0600

    23Oct

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -1,37 +1,75 @@
-import React, { useState, useRef } from "react";
-import ListItem from "../components/Listitem";
+import { useState, useRef, useEffect } from "react";
+import ListItem from "../components/ListItem";
+import { v4 as uuidv4 } from "uuid";
+import { data } from "autoprefixer";
 
 function Todo() {
+  // usestate -> {state, setstate}
   const [todos, setTodos] = useState([]);
   const inputRef = useRef(null);
 
+//DependencyList is null exec
+//useEffect(() =>{
+  //  console.log("useEffect");
+//} ,[])
+
+//without DependencyList exec aech time state changes
+//useEffect(() =>{
+  //  console.log("useEffect");
+//} ,)
+
+useEffect(() =>{
+   const getTodos = () =>{
+    fetch("https://jsonplaceholder.typicode.com/todos/1").then((response)=> response.json()).then((data) => {
+        console.log(data);
+    });
+   }
+} ,[])
+
+  // Add a new todo
   const addTodo = () => {
     const todoValue = inputRef.current.value;
-    if (todoValue) {
-      setTodos([...todos, todoValue]);
-      inputRef.current.value = ""; // Clear input field after adding a todo
-    }
+    const newTodo = { name: todoValue, id: uuidv4()}
+    console.log("before", todos);
+    setTodos([...todos, newTodo]);
+    console.log("after", todos)
+    inputRef.current.value = "";
   };
 
-  const handleDelete = (todoText) => {
-    setTodos(todos.filter((todo) => todo !== todoText));
+  const deleteTodo = (id) => {
+    // delete todo by id
+    // filter items different to given id
+    setTodos(todos.filter((item) => item.id !== id));
   };
 
   return (
     <div className="flex flex-col gap-2">
       <div className="flex gap-2">
-        <input ref={inputRef} type="text" className="rounded-md bg-slate-400" />
-        <button onClick={addTodo} className="rounded-md bg-indigo-600 px-4 py-2">
-          Add
+        <input
+          ref={inputRef}
+          type="text"
+          className="rounded-md bg-[#444] p-2"
+        />
+        <button
+          onClick={addTodo}
+          className="rounded-md bg-indigo-600 px-4 py-2"
+        >
+          Add ToDo
         </button>
       </div>
       <ul className="flex flex-col gap-2">
-        {todos.map((value, index) => {
-          return <ListItem key={index} text={value} onDelete={handleDelete} />;
+        {todos.map((item) => {
+          return (
+            <ListItem
+              key={item.id}
+              text={item.name}
+              onDelete={() => deleteTodo(item.id)}
+            />
+          );
         })}
       </ul>
     </div>
   );
 }
 
-export default Todo;
+export default Todo;
